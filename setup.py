c91077225a29badf4bb4d6231631dcd4a5e8c686
Stefan Davis <stefan@turnkeylinux.org>
2019-06-24 04:43:22 +0000

    Converted to pybuild package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="turnkey-sysinfo",
+    version="1.0",
+    author="Stefan Davis",
+    author_email="stefan@turnkeylinux.org",
+    url="https://github.com/turnkeylinux/turnkey-sysinfo",
+    packages=["lib-sysinfo"],
+    scripts=["turnkey-sysinfo"]
+)
