796d708059857e15f121ebd2e141ce8c4ed4d724
Jeremy Davis <jeremy@turnkeylinux.org>
2019-08-26 15:31:32 +1000

    consolidate and bugfix code (now that turnkey-version is included in pkg)

--- sysversion/__init__.py
+++ sysversion/__init__.py
@@ -1,59 +1,67 @@
 # Copyright (c) 2010 Liraz Siri <liraz@turnkeylinux.org>
 #
-# This file is part of turnkey-pylib.
+# This file is part of turnkey-sysinfo.
 #
-# turnkey-pylib is open source software; you can redistribute it and/or
+# turnkey-sysinfo is open source software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation; either version 3 of the
 # License, or (at your option) any later version.
 
-import os
 import re
-import subprocess
+from subprocess import run, PIPE
+from os.path import join
+from sys import stdin
 
 def _parse_turnkey_release(version):
     m = re.match(r'turnkey-.*?-(\d.*?)-[^\d]', version)
     if m:
         return m.group(1)
 
-def get_turnkey_release():
+def get_turnkey_release(rootfs='/'):
     """Return release_version. On error, returns None"""
-    try:
-        version = file("/etc/turnkey_version").read().strip()
-        return _parse_turnkey_release(version)
+    turnkey_version = get_turnkey_version(rootfs=rootfs)
+    if turnkey_version:
+        return _parse_turnkey_release(turnkey_version)
 
+def get_turnkey_version(rootfs='/'):
+    """Return turnkey_version. On error, returns None"""
+    try:
+        with open(join(rootfs, "etc/turnkey_version"), 'r') as fob:
+            return fob.read().strip()
     except IOError:
         pass
 
-def fmt_base_distribution():
+def fmt_base_distribution(encoding):
     """Return a formatted distribution string:
-        e.g., Ubuntu 8.04 Hardy LTS"""
+        e.g., Debian 10/Buster"""
 
-    try:
-        output = subprocess.check_output("lsb_release -ircd")
-    except subprocess.CalledProcessError:
+    process = run(["lsb_release", "-ircd"], stdout=PIPE)
+    if process.returncode != 0:
         return
 
-    d = dict([ line.split(':\t') 
-               for line in output.splitlines() ])
+    d = dict([line.split(':\t')
+              for line in process.stdout.decode(encoding).splitlines()])
 
     codename = d['Codename'].capitalize()
-    basedist = "%s %s %s" % (d['Distributor ID'],
-                             d['Release'],
-                             d['Codename'].capitalize())
-    if d['Codename'] in ('hardy', 'lucid'):
-        basedist += " LTS"
-
+    basedist = "{} {}/{}".format(d['Distributor ID'],
+                                 d['Release'],
+                                 d['Codename'].capitalize())
     return basedist
 
-def fmt_sysversion():
-    version = []
+def fmt_sysversion(encoding=stdin.encoding):
+    version_parts = []
     release = get_turnkey_release()
     if release:
-        version.append("TurnKey GNU/Linux %s" % release)
+        version_parts.append("TurnKey GNU/Linux %s" % release)
 
-    basedist = fmt_base_distribution()
+    basedist = fmt_base_distribution(encoding)
     if basedist:
-        version.append(basedist)
+        version_parts.append(basedist)
 
-    return ' / '.join(version)
+    if len(version_parts) == 2:
+        version = '{} ({})'.format(version_parts[0], version_parts[1])
+    elif len(version_parts) == 1:
+        version = version_parts[0]
+    else:
+        version = "Unknown"
+    return version
