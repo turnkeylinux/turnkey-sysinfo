059347b32d9ec7272fcf396d1b0aaa6441e4a14e
Jeremy Davis <jeremy@turnkeylinux.org>
2020-02-24 15:22:51 +1100

    Pre v16.0 release refactoring.
    Move turnkey-version and (library) sysversion back out into their own
    package again. This change was made to ease support for Debian in-place
    upgrade from v15.x to v16.x (with packages bundled together updating
    caused issues...).

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ from distutils.core import setup
 
 setup(
     name="turnkey-sysinfo",
-    version="1.0",
+    version="1.1",
     author="Stefan Davis",
     author_email="stefan@turnkeylinux.org",
     url="https://github.com/turnkeylinux/turnkey-sysinfo",
-    packages=["libsysinfo", "sysversion"],
-    scripts=["turnkey-sysinfo", "turnkey-version"]
+    packages=["libsysinfo"],
+    scripts=["turnkey-sysinfo"]
 )
